a35eebd51fb46bcc4769805532acc68431e40c7c
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-23 21:04:48 +0530

    Created q10.java
    Solutions to Euler q10 .

---
+++ q10.java
@@ -0,0 +1,23 @@
+package questions;
+
+public class q10 {
+
+  public static void main(String[] args) {
+    long i,sum=0;
+    int c=0;
+    for(i=1;i<=2000000;i++)
+    {
+      for(int j=2;j<i;j++)
+      {
+        if(i%j==0)
+        {
+          c++;
+        }
+        if(c==0)
+        sum=sum+i;
+      }
+    }
+    System.out.println(sum);
+  }
+
+}
