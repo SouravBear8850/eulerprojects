f9cc70fd0add0d7ee9a76cd68462a9c6393c9f6e
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-23 20:57:59 +0530

    Created q6.java
    Solutions to Euler questions6

---
+++ q6.java
@@ -0,0 +1,16 @@
+package questions;
+
+public class q6 {
+
+  public static void main(String[] args)
+   {int sum=0,sqsum=0,diffsum=0;
+      for(int i=1;i<101;i++)
+      {
+        sum=sum+i;
+        sqsum=sqsum+i*i;
+      }
+
+  } diffsum=sum*sum-sqsum;
+  System.out.println("difference of sum="+diffsum);
+
+}
