84a40aaae9857810f99d06cd943f791032739b3b
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-24 13:22:05 +0530

    Created q12.java
    Solution to Euler question 12

---
+++ q12.java
@@ -0,0 +1,32 @@
+package questions;
+
+public class q12 {
+
+  public int trianglenumber() {
+   long i;
+   
+   int sum,c;
+   for(i=1000;i>0;i++)
+   { sum=0;c=2;
+     for(int j=1;j<=i;j++)
+     {
+       sum=sum +i;
+     }
+     
+     for(int k=2;k<sum;k++)
+     {
+        if(sum%k==0)
+        {
+          c++;
+        }
+         if(c>=500 )
+         return i;
+     }
+   }
+  }
+ public void main(String args[])
+ {
+   long num=trianglenumber();
+   System.out.print("triangle numberwith 500 divisors is="+num);
+ }
+}
