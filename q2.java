848613368e0c903c3b1d5a0af6c18e18f27852f5
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-26 23:24:27 +0530

    Update q2.java

--- q2.java
+++ q2.java
@@ -3,18 +3,19 @@ package questions;
 public class q2 {
 
   public static void main(String[] args) {
-   long firstno=0,secno=2,result=4;
+   long firstno=0,secno=2,result=2;
      System.out.print(firstno+",");
      System.out.print(secno+",");
-     System.out.print(result+",");
      
+
+
      while(true)
      {
        result=firstno+secno;
        if(result>4000000)
        break;
        firstno=secno;
-       secno=firstno;
+       secno=result;
        System.out.print(result+",");
      }
      
