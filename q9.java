b5c1a129e214c43c2ded4ee1d825bd20cdaaf6a7
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-27 00:10:36 +0530

    Update q9.java

--- q9.java
+++ q9.java
@@ -3,14 +3,14 @@ package questions;
 public class q9 {
 
   public static void main(String[] args) {
-    int a=0,b=0,c=0;
+    int a=0,b=0; double c=0;
     int s=1000;
     for(a=1;a<s/3;a++)
     {
       for(b=a;b<s/2;b++)
       {
         c=s-a-b;
-        if(c*c=a*a+b*b){
+        if(c*c==a*a+b*b){
         
         System.out.println("phythagorean triplets are="+a+','+b+','+c',');
         break;}
