684b07f15a02227629bd7ae9f932cc6e47826f8b
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-28 23:11:33 +0530

    Updated q10.java

--- q10.java
+++ q10.java
@@ -2,15 +2,18 @@
 
 public class q10 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+{
     long i,sum=0;
     int c;
-    for(i=1;i<=2000000;i=i+2)
-    {c=0;
+    for(i=3;i<=2000000;i=i+2)
+    {
+      c=0;
       for(int j=2;j<=i/2;j++)
       {
         if(i%j==0)
         {
+          i=i/j;
           c++;
         }
         if(c==0)
