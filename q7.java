81aef3cc2bddae00cacc3ee0bc21357d1b06e6c8
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-29 22:54:46 +0530

    Updated q7.java

--- q7.java
+++ q7.java
@@ -2,7 +2,7 @@ public class q7 {
 
   public static void main(String[] args) {
     long i,num=0;
-    long c=5;
+    long c=4;
     for(i=11 ;i<=100000;i=i+2)
     {
       for(long j=3;j<=i/2;j=j+2)
