365e0b1e80d4d860a982fbc51322599cf5c8c8c4
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-29 19:38:30 +0530

    Update q4.java

--- q4.java
+++ q4.java
@@ -1,8 +1,8 @@
 public class q4 {
 
   public static void main(String[] args) {
-      int i=0;
-      long prod=1,nnum=0,placeval=1000000,dig=0,num=0;
+      int i=0,c=0;
+      long prod=1,nnum=0,placeval=100000,dig=0,num=0;
       for(i=999;i>=100;i--)
       {
         for(int j=999;j>=100;j--)
@@ -15,6 +15,7 @@ public class q4 {
             nnum=nnum+(dig*placeval);
             num=num/10;
             placeval/=10;
+             
           }
           if(prod==nnum)
           break a;
