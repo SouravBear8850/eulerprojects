710ed8d0cf87198c3d9e5adba9fcbe13ef76c76e
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-29 21:19:08 +0530

    Created q1.java

--- q1.java
+++ q1.java
@@ -4,7 +4,7 @@ public class q1 {
 
   public static void main(String[] args) {
     int sum=0;
-    for(int i=1;i<=100;i++)
+    for(int i=1;i<1000;i++)
       {
         if(i%3==0||i%5==0)
           { 
